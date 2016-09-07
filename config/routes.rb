aff366155f729b780ccc5429c77aad108da30b85
Timothy Dang <timothy.dang@gmail.com>
2016-09-07 16:18:13 -0700

    Update scema

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'welcome/index'
+
+  # the original rails default route
+  match ':controller(/:action(/:id))', :via => :get
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
